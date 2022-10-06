4ee2cf3013042fcecd2bb08aa01ddc573a735e54
supanut <supanut@gmail.com>
2022-10-06 23:59:27 +0700

    feat: adding links

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './routes/home/home.component';
+import Navigation from "./routes/navigation/navigation.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Home from "./routes/home/home.component";
 
 const Shop = () => {
-  return (
-    <h1>I am the shop page</h1>
-  )
-}
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
