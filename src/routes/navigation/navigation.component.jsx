d05f5bb639dd2ae6e3b3be845c40750936486877
supanut <supanut@gmail.com>
2022-10-10 18:18:29 +0700

    added sign in component as well as firebase utils and brought in firebase libraries

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -16,6 +16,9 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
+          <Link className="sign-in-link" to="/signin">
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw9YtY0Prj9-aIkjVekoF_fSys5sji-c8",
+  authDomain: "crawn-clothing-db-3b0cd.firebaseapp.com",
+  projectId: "crawn-clothing-db-3b0cd",
+  storageBucket: "crawn-clothing-db-3b0cd.appspot.com",
+  messagingSenderId: "4939747721",
+  appId: "1:4939747721:web:64efda09e2bfa8ec791158",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
